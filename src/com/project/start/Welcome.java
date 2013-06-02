739ecd4f58d7b202221c674148e0023302c78f32
TheCruzader <darkevafan@outlook.com>
2013-06-02 04:14:49 -0500

    Modified: Greeting

--- src/com/project/start/Welcome.java
+++ src/com/project/start/Welcome.java
@@ -7,7 +7,7 @@ public class Welcome {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		System.out.println("Hola a Todos!");
 	}
 
 }
