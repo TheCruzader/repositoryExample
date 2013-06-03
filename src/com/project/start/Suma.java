cc794d808ba6d42b6c1691fa5fe398ea551bc025
TheCruzader <darkevafan@outlook.com>
2013-06-02 20:47:24 -0500

    Modified: Saludos cambiados, agregado clase para sumar

---
+++ src/com/project/start/Suma.java
@@ -0,0 +1,14 @@
+package com.project.start;
+
+public class Suma {
+
+	/**
+	 * @param args
+	 */
+
+	public static int suma(int a, int b)
+	{
+		return a+ b;
+	}
+
+}
--- src/com/project/start/Welcome.java
+++ src/com/project/start/Welcome.java
@@ -7,7 +7,8 @@ public class Welcome {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola a Todos!");
+		System.out.println("Hola a Todos uds me caen muy bien!");
+		System.out.println("Esto es ahora 1 +1 =="+ Suma.suma(1,1));
 	}
 
 }
